1fbbbaeede18875bd15ffc61cd78e5d2b1ddfd55
Meiallu <53249955+Meiallu@users.noreply.github.com>
2024-04-02 15:48:04 -0300

    removed unecessary things

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java")
-    `maven-publish`
 }
 
 group = "me.meiallu.cabbadb"
@@ -17,16 +16,4 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("io.netty:netty-all:4.1.108.Final")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "me.meiallu.cabbadb"
-            artifactId = "client"
-            version = "0.1"
-
-            from(components["java"])
-        }
-    }
 }
